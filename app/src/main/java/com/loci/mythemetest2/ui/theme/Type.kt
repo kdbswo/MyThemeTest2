1e1edd0cfb656d70276764d5b64ca460409e3463
kdbswo <kdbswo@gmail.com>
2023-10-16 17:15:08 +0900

    Initial commit

---
+++ app/src/main/java/com/loci/mythemetest2/ui/theme/Type.kt
@@ -0,0 +1,24 @@
+package com.loci.mythemetest2.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontSize = 30.sp,
+        fontWeight = FontWeight.ExtraBold
+    ),
+    titleMedium = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleSmall = TextStyle(
+        fontSize = 10.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+
+)
